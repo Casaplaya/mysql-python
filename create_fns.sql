62985a68cb4ecae827daaa83d3c076ccddc34888
Aely Aronoff <aely.aro@gmail.com>
2022-01-20 17:17:50 -0800

    Add delete functions to create_fns.sql

--- create_fns.sql
+++ create_fns.sql
@@ -1,3 +1,9 @@
+DROP FUNCTION py_eval;
+DROP FUNCTION py_hex_to_dec;
+DROP FUNCTION py_hex_with_dec;
+DROP FUNCTION py_hex_sum;
+DROP FUNCTION py_hex_sum_with_dec;
+
 CREATE FUNCTION py_eval RETURNS STRING SONAME 'py_eval.so';
 CREATE FUNCTION py_hex_to_dec RETURNS STRING SONAME 'py_eval.so';
 CREATE FUNCTION py_hex_with_dec RETURNS STRING SONAME 'py_eval.so';
